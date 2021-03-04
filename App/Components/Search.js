b9cd3c1478edaba5e63e6221db337b4c97b70613
Jimmy Kane <jbk5603@psu.edu>
2021-03-03 22:18:57 -0500

    Got Search to work

--- App/Components/Search.js
+++ App/Components/Search.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Button, TextInput, TouchableOpacity, TouchableWithoutFeedback, Platform, Keyboard } from 'react-native'
 import { Metrics, Colors } from '../Themes'
 import { SearchBar } from 'react-native-elements'
 
 export default function Search(props) {
 	const [text, setText] = useState("");
+
+	useEffect(() => {
+		console.log(text)
+	}, [text])
+
 	return (
 		<View>
-			<TextInput placeholder="Type Here...." onChangeText={text => setText(text)} value={text} onSubmitEditing={props.search}/>
+			<TextInput placeholder="Type Here...." onChangeText={text => setText(text)} value={text} onSubmitEditing={async () => {props.search(text); console.log("I was clicked")}}/>
 			{/* user input and a search button */}
 		</View>
 	)
